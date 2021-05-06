628236d18e490d8b66ca83b1119ee7bbca0a1aea
Athar Javed <47935439+atharjaved15@users.noreply.github.com>
2021-05-06 22:01:54 +0500

    Initial commit

---
+++ lib/splash.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'landing.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await getApplicationDocumentsDirectory().then((Directory directory) async {
+    File jsonFile = new File(directory.path + "/portfolio.json");
+    if (jsonFile.existsSync()) {
+      portfolioMap = json.decode(jsonFile.readAsStringSync());
+    } else {
+      jsonFile.createSync();
+      jsonFile.writeAsStringSync("{}");
+      portfolioMap = {};
+    }
+    if (portfolioMap == null) {
+      portfolioMap = {};
+    }
+    jsonFile = new File(directory.path + "/marketData.json");
+    if (jsonFile.existsSync()) {
+      marketListData = json.decode(jsonFile.readAsStringSync());
+    } else {
+      jsonFile.createSync();
+      jsonFile.writeAsStringSync("[]");
+      marketListData = [];
+      // getMarketData(); ?does this work?
+    }
+  });
+
+  String themeMode = "Automatic";
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  if (prefs.getBool("shortenOn") != null &&
+      prefs.getString("themeMode") != null) {
+    shortenOn = prefs.getBool("shortenOn");
+    themeMode = prefs.getString("themeMode");
+  }
+
+  runApp(splashScreen());
+}
+
+class splashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: AnimatedSplashScreen(
+          nextScreen: TraceApp('Automatic', shortenOn),
+          splash: Image.asset('assets/icon.png'),
+          splashTransition: SplashTransition.slideTransition,
+          splashIconSize: 350,
+        ),
+      ),
+    );
+  }
+}
